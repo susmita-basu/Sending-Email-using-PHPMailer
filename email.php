0f8aaeeed90e1a2c0d35719f46092a6c0b09a159
susmita-basu <76273236+susmita-basu@users.noreply.github.com>
2020-12-28 23:36:46 +0530

    Update email.php

--- email.php
+++ email.php
@@ -16,16 +16,16 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                                                         // Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                                                   // Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                                              // Enable SMTP authentication
-$mail->Username   = 'susmitabasumallick07@gmail.com';                                 // SMTP username
-$mail->Password   = 'fatherreturninghome';                                           // SMTP password
+$mail->Username   = '';                                 // SMTP username
+$mail->Password   = '';                                           // SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                                                          // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port       = 587;                                                           // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 //Recipients
-$mail->setFrom('susmitabasumallick07@gmail.com','Susmita Basu Mallick');                                                                                 // sent from xyz mail
-$mail->addAddress('susmitabasumallick51@gmail.com');                                                                   // sent to xyz mail
+$mail->setFrom('','Susmita Basu Mallick');                                                                                 // sent from xyz mail
+$mail->addAddress('');                                                                   // sent to xyz mail
 //$mail->addAddress('ellen@example.com');                                                                                                 // Name is optional
-$mail->addReplyTo('susmitabasumallick07@gmail.com');
+$mail->addReplyTo('');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 
