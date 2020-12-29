8b14c81cb8aa8941ab6646fcf1f99b3c06f5642e
susmita-basu <76273236+susmita-basu@users.noreply.github.com>
2020-12-29 10:36:37 +0530

    Update email.php

--- email.php
+++ email.php
@@ -16,22 +16,22 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                                                         // Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                                                   // Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                                              // Enable SMTP authentication
-$mail->Username   = '';                                 // SMTP username
-$mail->Password   = '';                                           // SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                                                          // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+$mail->Username   = '';                                                               // SMTP username
+$mail->Password   = '';                                                              // SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                                 // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port       = 587;                                                           // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 //Recipients
-$mail->setFrom('','Susmita Basu Mallick');                                                                                 // sent from xyz mail
-$mail->addAddress('');                                                                   // sent to xyz mail
-//$mail->addAddress('ellen@example.com');                                                                                                 // Name is optional
+$mail->setFrom('','Susmita Basu Mallick');                                       // sent from xyz mail
+$mail->addAddress('');                                                          // sent to xyz mail
+//$mail->addAddress('ellen@example.com');                                      // Name is optional
 $mail->addReplyTo('');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 
 // Attachments
-//$mail->addAttachment('/var/tmp/file.tar.gz');                                                                                         // Add attachments
-$mail->addAttachment('image/panda.jpg', 'Panda.jpg');                                                                                   // Optional name
+//$mail->addAttachment('/var/tmp/file.tar.gz');                            // Add attachments
+$mail->addAttachment('image/panda.jpg', 'Panda.jpg');                     // Optional name
 
 // Content
 $mail->isHTML(true);                                                                                                                 // Set email format to HTML
